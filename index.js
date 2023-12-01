a0664976661c1db83f6667ef969159c44afefe23
KixtyTakodachi <sanya.kim.sk@gmail.com>
2023-12-01 14:20:10 +0600

    env to gitignore + revert uploads fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express()
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-    if(!fs.existsSync('/uploads')) {
+    if(!fs.existsSync('uploads')) {
       fs.mkdirSync('uploads')
     }
     cb(null, 'uploads')
